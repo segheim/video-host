36316cfb4637b9a93574152248e837135de3ade0
Evgeni Savitsky <evgeni.savitsky@rndpoint.com>
2023-11-25 19:21:29 +0300

    init

---
+++ src/main/java/com/example/videohost/VideoHostApplication.java
@@ -0,0 +1,13 @@
+package com.example.videohost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoHostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoHostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/videohost/VideoHostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videohost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
